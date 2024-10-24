c69e5397c09fe6a31912131f13ed02354b1692d4
Khan <loprommy55@gmail.com>
2024-10-24 12:30:24 +0700

    first commmit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Navbar/>
+        <div>
+          {children}
+        </div>
+      <Footer/>
+    </>
+
+  )
+}
+
+export default Layout
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const About = () => {
+  return (
+    <Layout>
+            <div>
+                About
+            </div>
+    </Layout>
+
+  )
+}
+
+export default About
